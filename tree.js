ad49db44c30abb0debbc572d17ca760d73ee57df
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-06-14 21:02:20 +0200

    Add prettyPrint method to showcase the tree in a tree like view

---
+++ tree.js
@@ -0,0 +1,18 @@
+import { buildTree } from './balanced_binary_search.js';
+
+function Tree(arr) {
+  let root = buildTree(arr);
+
+  const prettyPrint = (node = root, prefix = '', isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+}
