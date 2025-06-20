da34828146c79b4daadd21e563908cf8c04c8496
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-06-20 19:10:27 +0200

    Add method that check the balance of the tree

--- tree.js
+++ tree.js
@@ -207,6 +207,28 @@ function Tree(arr) {
     return computeDepth(root, value);
   };
 
+  const isBalanced = () => {
+    if (root === null) return true;
+
+    const checkHeight = (node) => {
+      if (node === null) return 0;
+
+      const leftHeight = checkHeight(node.left);
+      const rightHeight = checkHeight(node.right);
+
+      if (
+        leftHeight === -1 ||
+        rightHeight === -1 ||
+        Math.abs(leftHeight - rightHeight) > 1
+      )
+        return -1;
+
+      return Math.max(leftHeight, rightHeight) + 1;
+    };
+
+    return checkHeight(root) !== -1;
+  };
+
   return {
     prettyPrint,
     insert,
